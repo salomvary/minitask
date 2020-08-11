4462a534707b0bde82b038806a34e4bf82d2223f
Márton Salomváry <salomvary@gmail.com>
2020-08-11 14:50:02 +0200

    Add project list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from .models import Task
 
 
 def index(request):
-    return HttpResponse(render_to_string("index.html", {"user": request.user}))
+    tasks = Task.objects.all()
+    return HttpResponse(render_to_string("index.html", {"user": request.user, "tasks": tasks}))
