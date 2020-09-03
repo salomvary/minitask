e61d8dc7b619f2e671fa3cf250f7a1616240d65f
Márton Salomváry <salomvary@gmail.com>
2020-09-03 07:46:01 +0200

    Customize and translate all admin titles

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Note, Project, ProjectMembership, Task, User
+
+# Text to put at the end of each page's <title>.
+admin.site.site_title = _("Minitask administration")
+# Text to put in each page's <h1> (and above login form).
+admin.site.site_header = _("Minitask administration")
+# Text to put at the top of the admin index page.
+admin.site.index_title = None
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Project)
--- tasks/apps.py
+++ tasks/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class TasksConfig(AppConfig):
     name = "tasks"
+    verbose_name = _("tasks")
